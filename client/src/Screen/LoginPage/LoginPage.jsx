26a313c592d403616f130e01c94d757621f5ba86
santhoshkumarr.231299@gmail.com <santhoshkumarr.231299@gmail.com>
2023-02-14 01:15:09 +0530

    added bootstrap styles

--- client/src/Screen/LoginPage/LoginPage.jsx
+++ client/src/Screen/LoginPage/LoginPage.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Button, Form, Card } from "react-bootstrap";
+import { Button, Form, Card, Alert } from "react-bootstrap";
 
 function LoginPage() {
+  const [alertType, setAlertType] = useState();
+  const [alert, setAlert] = useState();
+  const [openAlert, setOpenAlert] = useState(false);
+
   const username = useRef();
   const password = useRef();
 
@@ -28,9 +32,11 @@ function LoginPage() {
       })
       .then((res) => {
         if (res.data.message === "success") {
-          // navigate("/");
+          navigate("/");
         } else {
-          document.getElementById("alert").innerHTML = "Login Failed";
+          setAlertType("danger");
+          setAlert(() => "Failed to Login");
+          setOpenAlert(() => true);
         }
       });
   };
@@ -41,51 +47,86 @@ function LoginPage() {
       labelName: "Username",
       type: "text",
       required: true,
+      reference: username,
     },
     {
       fieldName: "password",
       labelName: "Password",
       type: "password",
       required: true,
+      reference: password,
     },
   ];
 
   return (
-    <div className="container">
+    <div
+      style={{
+        marginTop: "10%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Card
         style={{
           minHeight: "250px",
-          maxWidth: "400px",
+          minWidth: "250px",
         }}
       >
-        <Form
+        <Card.Title
           style={{
-            margin: "2vh",
+            marginTop: "20px",
+            textAlign: "center",
           }}
         >
-          {fields.map((field) => (
-            <Form.Group className="mb-3" controlId={field.fieldName}>
-              <Form.Label>{field.labelName}</Form.Label>
-              <Form.Control
-                type={field.type}
-                ref={field.fieldName === "username" ? username : password}
-                placeholder={field.labelName}
-              />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
-          ))}
-          <Button
-            variant="primary"
-            type="submit"
-            onClick={(e) => validateLogin(e)}
+          Sign In
+        </Card.Title>
+        <Card.Body>
+          <Form
+            style={{
+              margin: "2vh",
+              marginTop: 0,
+            }}
           >
-            LOGIN
-          </Button>
-          <div>
-            <a href="#">Forgot your password</a>
-          </div>
-          <span id="alert"></span>
-        </Form>
+            {fields.map((field) => (
+              <Form.Group className="mb-3" controlId={field.fieldName}>
+                <Form.Label>{field.labelName}</Form.Label>
+                <Form.Control
+                  type={field.type}
+                  ref={field.reference}
+                  placeholder={field.labelName}
+                />
+                <Form.Text className="text-muted"></Form.Text>
+              </Form.Group>
+            ))}
+            <Alert
+              variant={alertType}
+              style={{
+                display: openAlert ? "" : "none",
+              }}
+            >
+              {alert}
+            </Alert>
+            <Button
+              style={{
+                width: "100%",
+              }}
+              variant="primary"
+              type="submit"
+              onClick={(e) => validateLogin(e)}
+            >
+              Log In
+            </Button>
+            <div
+              style={{
+                margin: "10px",
+                textAlign: "center",
+              }}
+            >
+              <a onClick={0}>Forgot your password</a>
+            </div>
+          </Form>
+        </Card.Body>
       </Card>
     </div>
   );
--- client/src/Screen/NewUserPage/NewUserPage.jsx
+++ client/src/Screen/NewUserPage/NewUserPage.jsx
@@ -1,78 +1,146 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import React, { Component } from "react";
+import { Button, Form, Card, Alert } from "react-bootstrap";
 
-class NewUserPage extends Component {
-  fields = [
+function NewUserPage() {
+  const [alertType, setAlertType] = useState();
+  const [alert, setAlert] = useState();
+  const [openAlert, setOpenAlert] = useState(false);
+
+  const username = useRef();
+  const password = useRef();
+  const email = useRef();
+  const phoneNumber = useRef();
+
+  const navigate = useNavigate();
+
+  const fields = [
     {
-      name: "username",
+      fieldName: "username",
       type: "text",
-      pname: "Username",
-      iconType: "user",
+      labelName: "Username",
+      required: true,
+      reference: username,
     },
     {
-      name: "password",
+      fieldName: "password",
       type: "password",
-      pname: "Password",
-      iconType: "lock",
+      labelName: "Password",
+      required: true,
+      reference: password,
     },
     {
-      name: "phone",
+      fieldName: "phone",
       type: "number",
-      pname: "Phone Number",
-      iconType: "mobile",
+      labelName: "Phone Number",
+      required: true,
+      reference: phoneNumber,
     },
     {
-      name: "email",
+      fieldName: "email",
       type: "email",
-      pname: "Email",
-      iconType: "inbox",
+      labelName: "Email",
+      required: true,
+      reference: email,
     },
   ];
-  createUser = (e) => {
+  const createUser = (e) => {
     e.preventDefault();
     const user = {
-      username: document.getElementById("username").value,
-      password: document.getElementById("password").value,
+      username: username.current.value,
+      password: password.current.value,
     };
+    console.log(user);
     axios.post("http://localhost:3000/new-user", user).then((resp) => {
       if (resp.data) {
         if (resp.data.message === "success") {
-          window.alert("New User Created Successfully");
+          setAlertType("success");
+          setAlert(() => "New User Created Successfully");
+          setOpenAlert(true);
         } else {
-          window.alert("failed");
+          setAlertType("danger");
+          setAlert(() => "Failed to Create User");
+          setOpenAlert(true);
         }
       }
     });
   };
-  render() {
-    return (
-      <div className="container">
-        <form>
-          <div>
-            {this.fields.map((field) => (
-              <div>
-                <input
-                  id={field.name}
+  const goToLoginPage = () => {
+    navigate("/login");
+  };
+  return (
+    <div
+      style={{
+        marginTop: "10%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        style={{
+          minHeight: "250px",
+          minWidth: "250px",
+        }}
+      >
+        <Card.Title
+          style={{
+            marginTop: "20px",
+            textAlign: "center",
+          }}
+        >
+          Sign Up
+        </Card.Title>
+        <Card.Body>
+          <Form
+            style={{
+              margin: "2vh",
+              marginTop: 0,
+            }}
+          >
+            {fields.map((field) => (
+              <Form.Group className="mb-3" controlId={field.fieldName}>
+                <Form.Label>{field.labelName}</Form.Label>
+                <Form.Control
                   type={field.type}
-                  placeholder={field.pname}
-                  required
+                  ref={field.reference}
+                  placeholder={field.labelName}
                 />
-                <br />
-              </div>
+                <Form.Text className="text-muted"></Form.Text>
+              </Form.Group>
             ))}
-            <button
-              type="submit"
-              className="btn btn-secondary btn-block"
-              onClick={(e) => this.createUser(e)}
+            <Alert
+              variant={alertType}
+              style={{
+                display: openAlert ? "" : "none",
+              }}
             >
-              CREATE ACCOUNT
-            </button>
-          </div>
-        </form>
-        <span id="alert"></span>
-      </div>
-    );
-  }
+              {alert}
+            </Alert>
+            <Button
+              style={{
+                width: "100%",
+              }}
+              variant="primary"
+              type="submit"
+              onClick={(e) => createUser(e)}
+            >
+              Create Account
+            </Button>
+            <div
+              style={{
+                margin: "10px",
+                textAlign: "center",
+              }}
+            >
+              <a onClick={() => goToLoginPage()}>Already a user?</a>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default NewUserPage;
