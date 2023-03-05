1c2529d0ed15afeb3478aa2eae500f5de42e311e
santhoshkumarr.231299@gmail.com <santhoshkumarr.231299@gmail.com>
2023-03-05 12:37:47 +0530

    updated user previleges page

--- client/src/Screen/AssignUserPrevileges/AssignUserPrevileges.jsx
+++ client/src/Screen/AssignUserPrevileges/AssignUserPrevileges.jsx
@@ -5,7 +5,7 @@ import { Form } from "react-bootstrap";
 import { Paper, Button, TextField } from "@mui/material";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-// import Typeahead from "react-bootstrap-typeahead";
+import { Typeahead } from "react-bootstrap-typeahead";
 
 export default function AssignUserPrevilegesPage(props) {
   return (
@@ -42,82 +42,74 @@ function AssignUserPrevileges(props) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
   });
 
-  const [selectedUser, setSelectedUser] = useState("Pharmacist2");
+  const [selectedUser, setSelectedUser] = useState([]);
   const [users, setUsers] = useState([]);
   const [usePrevileges, setUserPrevileges] = useState("");
 
   const allMenus = [
     {
-      FieldId: 1,
+      FieldId: "[1]",
       FieldName: "Dashboard",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[1]"),
     },
     {
-      FieldId: 2,
+      FieldId: "[2]",
       FieldName: "Invoice",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[2]"),
     },
     {
-      FieldId: 3,
+      FieldId: "[3]",
       FieldName: "Customer",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[3]"),
     },
     {
-      FieldId: 4,
+      FieldId: "[4]",
       FieldName: "Medicine",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[4]"),
     },
     {
-      FieldId: 5,
+      FieldId: "[5]",
       FieldName: "Pharmacist",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[5]"),
     },
     {
-      FieldId: 6,
+      FieldId: "[6]",
       FieldName: "Delivery Man",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[6]"),
     },
     {
-      FieldId: 7,
+      FieldId: "[7]",
       FieldName: "Sales Report",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[7]"),
     },
     {
-      FieldId: 8,
+      FieldId: "[8]",
       FieldName: "Purchase",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[8]"),
     },
     {
-      FieldId: 9,
+      FieldId: "[9]",
       FieldName: "Reports",
       FieldType: "checkbox",
       isChecked: usePrevileges && usePrevileges.includes("[9]"),
     },
   ];
-  const getSearchedUser = (e) => {
-    e.preventDefault();
-    axios
-      .post("http://localhost:3000/get-users", { search: e.target.value })
-      .then((resp) => {
-        setSeverity(resp.data.status);
-        setMessage(resp.data.message);
-        setOpen(true);
-        setUsers(resp.data.users);
-      })
-      .catch((err) => {
-        setSeverity("error");
-        setMessage("Something went wrong");
-        setOpen(true);
-      });
+  const changeOption = (e, id) => {
+    if (!e.target.checked) {
+      setUserPrevileges(() => usePrevileges.replace(id, ""));
+    } else {
+      setUserPrevileges(() => usePrevileges + id);
+    }
   };
+
   useEffect(() => {
     axios
       .post("http://localhost:3000/get-users", { search: "" })
@@ -125,7 +117,12 @@ function AssignUserPrevileges(props) {
         setSeverity(resp.data.status);
         setMessage(resp.data.message);
         setOpen(true);
-        setUsers(resp.data.users);
+        let userOpt = [];
+        resp.data.users.map((user) => {
+          userOpt.push({ label: user.username });
+        });
+        setUsers(userOpt);
+        console.log(userOpt);
       })
       .catch((err) => {
         setSeverity("error");
@@ -135,11 +132,10 @@ function AssignUserPrevileges(props) {
   }, []);
 
   useEffect(() => {
-    selectedUser &&
-      selectedUser !== "" &&
+    if (selectedUser && selectedUser.length === 1) {
       axios
         .post("http://localhost:3000/get-user-previleges", {
-          username: selectedUser,
+          username: selectedUser[0].label,
         })
         .then((resp) => {
           setUserPrevileges(resp.data.userPrevileges);
@@ -149,38 +145,63 @@ function AssignUserPrevileges(props) {
           setMessage("Something went wrong");
           setOpen(true);
         });
+    } else {
+      setUserPrevileges("");
+    }
   }, [selectedUser]);
 
   return (
     <div>
       <Form>
-        <Form.Group>
-          <Form.Label>Search User by Username</Form.Label>
-          {/* <Typeahead
-            labelKey="name"
-            onChange={(e) => getSearchedUser(e)}
+        <Form.Group
+          style={{ maxWidth: "400px", margin: "auto", padding: "20px" }}
+        >
+          <Typeahead
+            id="typeahead"
+            labelKey="label"
+            onChange={setSelectedUser}
             options={users}
             placeholder="Select a User..."
             selected={selectedUser}
-          /> */}
+          />
         </Form.Group>
+        <h6
+          style={{
+            margin: "10px",
+            display: "flex",
+            gap: "5px",
+            margin: "auto",
+            justifyContent: "center",
+          }}
+        >
+          <div>Selected User : </div>
+          <div>
+            {selectedUser && selectedUser.length === 1
+              ? selectedUser[0].label
+              : "none"}
+          </div>
+        </h6>
         <div
           style={{
-            width: "100px",
+            padding: "20px",
+            width: "200px",
             margin: "auto",
             textAlign: "left",
             display: selectedUser && selectedUser === "" ? "none" : "block",
           }}
         >
-          {allMenus.map((menu) => (
-            <div>
-              <Form.Check
-                type={menu.FieldType}
-                label={menu.FieldName}
-                checked={menu.isChecked}
-              />
-            </div>
-          ))}
+          {selectedUser.length > 0 &&
+            allMenus.map((menu) => (
+              <div>
+                <Form.Check
+                  id={menu.FieldName}
+                  onChange={(e) => changeOption(e, menu.FieldId)}
+                  type={menu.FieldType}
+                  label={menu.FieldName}
+                  checked={menu.isChecked}
+                />
+              </div>
+            ))}
         </div>
       </Form>
       <Button
